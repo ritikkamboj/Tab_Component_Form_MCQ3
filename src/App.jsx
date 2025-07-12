fbe38e67261542152f90d55ce28122286b5a4b80
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-12 16:55:24 +0530

    our approach is configure driven in which we gets components dynamically from the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react'
 import './App.css'
+import Profile from './Profile'
+import Interests from './Interests'
+import Settings from './Settings'
+import { useState } from 'react'
 
 function App() {
 
+  const [activeTab , setActiveTab] = useState(1)
+
+  const tabs = [{
+    name: "profile",
+    component: Profile
+  }, {
+    name: "interest",
+    component: Interests
+  },
+  {
+    name: "setting",
+    component: Settings
+  }]
+let ActiveComponent = tabs[activeTab].component ;
+
+
   return (
     <>
-     <h1>Hello world</h1>
+      <h1 className='heading-main'>Tab Form </h1>
+      <div className='heading-container'>
+
+        {tabs.map((item) => <div className='heading' key={item.name}> {item.name}</div>)}
+
+      </div>
+      <div>
+        <ActiveComponent/>
+
+      </div>
+
+
+
     </>
   )
 }
