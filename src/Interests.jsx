102f1a080e0cbdc688f909647c4a968ebb1a2114
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-12 18:45:46 +0530

    handle the radio button in tab

--- src/Interests.jsx
+++ src/Interests.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
-function Interests() {
+function Interests({data , setData}) {
+     const {interests} = data ;
   return (
-    <div>Interests</div>
+   <>
+   <h1>Interests</h1>
+   
+   <label >
+    <input type="checkbox" name ="coding" checked={interests.includes("coding")}/>
+    Coding
+   </label>
+   
+   <label >
+    <input type="checkbox" name ="music" checked={interests.includes("music")}/>
+    Music
+   </label>
+   
+   <label >
+    <input type="checkbox" name ="javascript" checked={interests.includes("javascript")}/>
+    Javascript
+   </label>
+   
+   </>
+
+
   )
 }
 
--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
-function Settings() {
+function Settings({data , setData}) {
+
+    const {settings} = data
+
+    function handleRadio(e)
+    {
+        setData((prevState) => {
+            return {
+                ...prevState , settings : e.target.name
+            }
+        })
+
+
+    }
   return (
-    <div>Settings</div>
+    <>
+    <h1>Settings</h1>
+    <div>
+        <label>
+            <input type="radio" name='dark' checked={settings=== "dark"} onChange={handleRadio} />
+            Dark
+        </label>
+           <label>
+            <input type="radio" name='light' checked={settings=== "light"} onChange={handleRadio} />
+            Light
+        </label>
+    </div>
+    </>
   )
 }
 
