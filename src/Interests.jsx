ef19a32669e539b800dbe434a9d1914ab2db7e52
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-12 18:54:22 +0530

    set the inerests array (nice logic)

--- src/Interests.jsx
+++ src/Interests.jsx
@@ -2,22 +2,34 @@ import React from 'react'
 
 function Interests({data , setData}) {
      const {interests} = data ;
+
+     function handleInterests(e)
+     {
+        setData((prevState)=> {
+            return {
+                ...prevState , interests : e.target.checked ? [...prevState.interests , e.target.name] : prevState.interests.filter((item)=> item !== e.target.name)
+            }
+        })
+
+     }
+
+    //  console.log(interests)
   return (
    <>
    <h1>Interests</h1>
    
    <label >
-    <input type="checkbox" name ="coding" checked={interests.includes("coding")}/>
+    <input type="checkbox" name ="coding" checked={interests.includes("coding")}  onChange={handleInterests}/>
     Coding
    </label>
    
    <label >
-    <input type="checkbox" name ="music" checked={interests.includes("music")}/>
+    <input type="checkbox" name ="music" checked={interests.includes("music")}  onChange={handleInterests}/>
     Music
    </label>
    
    <label >
-    <input type="checkbox" name ="javascript" checked={interests.includes("javascript")}/>
+    <input type="checkbox" name ="javascript" checked={interests.includes("javascript")}  onChange={handleInterests}/>
     Javascript
    </label>
    
